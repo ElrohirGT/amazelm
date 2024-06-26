bfc8e276304ae3ff15a1b91b343ecc90e95d9d46
ElrohirGT <elrohirgt@gmail.com>
2024-06-26 13:01:49 -0600

    feat: Added flake dependencies

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    forAllSystems = {
+      pkgs ? nixpkgs,
+      function,
+    }:
+      nixpkgs.lib.genAttrs [
+        "x86_64-linux"
+        "x86_64-macos"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ]
+      (system:
+        function {
+          pkgs = import pkgs {
+            inherit system;
+            config.allowUnfree = true;
+            overlays = [
+              #inputs.something.overlays.default
+            ];
+          };
+          inherit system;
+        });
+  in {
+    devShells = forAllSystems {
+      function = {
+        pkgs,
+        system,
+      }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            elmPackages.elm
+          ];
+        };
+      };
+    };
+  };
+}
