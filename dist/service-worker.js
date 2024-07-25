abcb583462483d71be34b2c2febd8c4277cd8cd3
ElrohirGT <elrohirgt@gmail.com>
2024-07-25 12:16:27 -0600

    feat: Added logs

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -10,32 +10,33 @@ const filesToCache = [
 
 self.addEventListener("install", e => {
   console.log("[ServiceWorker] - Install");
-  e.waitUntil((async () => {
-    const cache = await caches.open(cacheName);
-    console.log("[ServiceWorker] - Caching app shell");
-    await cache.addAll(filesToCache);
-  })());
+  // e.waitUntil((async () => {
+  //   const cache = await caches.open(cacheName);
+  //   console.log("[ServiceWorker] - Caching app shell");
+  //   await cache.addAll(filesToCache);
+  // })());
 });
 
 self.addEventListener("activate", e => {
-  e.waitUntil((async () => {
-    // Get a list of all your caches in your app
-    const keyList = await caches.keys();
-    await Promise.all(
-      keyList.map(key => {
-        console.log(key);
-        /* 
-           Compare the name of your current cache you are iterating through
-           and your new cache name
-        */
-        if (key !== cacheName) {
-          console.log("[ServiceWorker] - Removing old cache", key);
-          return caches.delete(key);
-        }
-      })
-    );
-  })());
-  e.waitUntil(self.clients.claim());
+  console.log("[ServiceWorker] - Activate");
+  // e.waitUntil((async () => {
+  //   // Get a list of all your caches in your app
+  //   const keyList = await caches.keys();
+  //   await Promise.all(
+  //     keyList.map(key => {
+  //       console.log(key);
+  //       /* 
+  //          Compare the name of your current cache you are iterating through
+  //          and your new cache name
+  //       */
+  //       if (key !== cacheName) {
+  //         console.log("[ServiceWorker] - Removing old cache", key);
+  //         return caches.delete(key);
+  //       }
+  //     })
+  //   );
+  // })());
+  // e.waitUntil(self.clients.claim());
 });
 
 // service-worker.js
